2a1982070f7526160e545600f3ef433294c85692
Bertrik Sikken <bertrik@sikken.nl>
2020-04-11 21:16:29 +0200

    Show accelerometer data as a color.

--- atommatrix.ino
+++ atommatrix.ino
@@ -3,9 +3,12 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
+#include "MPU6886.h"
+
 #define DATA_PIN_LED 27
 
 static CRGB leds[25];
+static MPU6886 mpu;
 
 void setup(void)
 {
@@ -14,19 +17,26 @@ void setup(void)
 
     FastLED.addLeds < WS2812B, DATA_PIN_LED, GRB > (leds, 25);
     FastLED.setBrightness(20);
+
+    mpu.Init();
+}
+
+static int accel_to_color(float a)
+{
+    int v = 128 + 127 * a;
+    return (v < 0) ? 0 : (v > 255) ? 255 : v;
 }
 
 void loop(void)
 {
-    static uint8_t hue = 0;
-    static uint32_t last_time = 0;
+    float x, y, z;
 
-    uint32_t time = millis() / 50;
-    if (time != last_time) {
-        last_time = time;
-        hue++;
-    }
+    mpu.getAccelData(&x, &y, &z);
+    Serial.printf("%f %f %f\n", x, y, z);
 
-    CRGB color = CHSV(hue, 255, 255);
+    int r = accel_to_color(x);
+    int g = accel_to_color(y);
+    int b = accel_to_color(z);
+    CRGB color = CRGB(r, g, b);
     FastLED.showColor(color);
 }
